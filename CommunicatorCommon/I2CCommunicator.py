d37a0e8f0f4dd5468058bc70b3201e22d63693a1
JarredTD <jarred.desrosiers@yahoo.com>
2023-09-21 11:23:10 -0400

    Update send/recv methods to account for multiple devices

--- CommunicatorCommon/I2CCommunicator.py
+++ CommunicatorCommon/I2CCommunicator.py
@@ -11,12 +11,11 @@ class I2CCommunicator:
 
     Attributes:
         busId (int): The I2C bus ID.
-        address (int): The I2C device address.
     """
 
     logger = logging.getLogger(__name__)
 
-    def __init__(self, busId: int, address: int) -> None:
+    def __init__(self, busId: int = 1) -> None:
         """
         Initialize the I2CCommunicator.
 
@@ -25,10 +24,9 @@ class I2CCommunicator:
             address (int): The I2C device address.
         """
         self._busId = busId
-        self._address = address
         self._bus = smbus2.SMBus(busId)
 
-    async def sendMsg(self, msg: bytes) -> None:
+    async def sendMsg(self, address: int, msg: bytes) -> None:
         """
         Send a message via I2C.
 
@@ -38,12 +36,12 @@ class I2CCommunicator:
         self.logger.info("Sending data: %s", msg)
         try:
             await asyncio.to_thread(
-                self._bus.write_i2c_block_data, self._address, 0, list(msg)
+                self._bus.write_i2c_block_data, address, 0, list(msg)
             )
         except Exception as e:
             self.logger.error("Error sending data: %s", str(e))
 
-    async def receiveMsg(self, length: int) -> bytes:
+    async def receiveMsg(self, address: int, length: int) -> bytes:
         """
         Receive data of the specified length via I2C.
 
@@ -56,7 +54,7 @@ class I2CCommunicator:
         self.logger.info("Receiving data of length %d", length)
         try:
             receivedMsg = await asyncio.to_thread(
-                self._bus.read_i2c_block_data, self._address, 0, length
+                self._bus.read_i2c_block_data, address, 0, length
             )
             return bytes(receivedMsg)
         except Exception as e:
