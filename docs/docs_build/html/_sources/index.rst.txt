9fe4884c6b638e88fdcb0d08a6a40dec62b7d227
JarredTD <jarred.desrosiers@yahoo.com>
2024-04-20 19:33:10 -0400

    refactor: Move doc generation html into docs dir

---
+++ docs/docs_build/html/_sources/index.rst.txt
@@ -0,0 +1,9 @@
+Welcome to CommunicationCommon's documentation!
+===============================================
+
+.. toctree::
+   :maxdepth: 2
+
+
+   src/serial
+   src/i2c
--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -11,6 +11,6 @@ make clean
 make html 2>&1 | tee "../$SPHINX_PATH/sphinx_output.txt"
 cd ..
 
-open $SPHINX_PATH/html/index.html
+open docs/docs_build/html/index.html
 
 deactivate
